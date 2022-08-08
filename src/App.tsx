3a0eb3ed2d1b1710531955828e49f5fbc3cd15e6
Elona Kajtazi <MY_NAME@example.com>
2022-08-08 15:06:12 +0200

    it looks terrible!!! but I am working on it!

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,31 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <section className="left-menu">
+        <div className="logo">Logo</div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">
+                <img src="/" alt="home" />
+                <h2>Home</h2>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+      <section className="main">
+        <h2>Main Content</h2>
+      </section>
+      <section className="right-menu">
+        <h2>Right Menu</h2>
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
